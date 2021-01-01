f16127be23603c5e34409613a41e95f1498e83f3
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-01 11:38:41 -0300

    add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next'
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from './themes';
+
 import './i18n';
 
 import './App.css';
 
-
 function App() {
   const { t } = useTranslation();
   // i18n.changeLanguage('en') // to change language
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
   return (
-    <div className="App">
-      <h1>{t('Welcome to React')}</h1>
-    </div>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <div className="App">
+        <h1>{t('Welcome to React')}</h1>
+        <button onClick={toggleTheme}>Change Theme</button>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const lightTheme = {
+  body: 'white',
+  fontColor: 'black',
+};
+
+export const darkTheme = {
+  body: 'black',
+  fontColor: 'white',
+};
+
+interface ITheme {
+  theme: { body: string; fontColor: string };
+}
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${(props: ITheme) => props.theme.body};
+    color: ${(props: ITheme) => props.theme.fontColor};
+    transition: all 500ms;
+  }
+`;
