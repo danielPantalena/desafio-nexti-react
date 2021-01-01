414d6f2068870de5b4b3ae5548e19ac8589e653c
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-01 10:58:29 -0300

    initial commit
    add i18n for translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useTranslation } from 'react-i18next'
+import './i18n';
+
 import './App.css';
 
+
 function App() {
+  const { t } = useTranslation();
+  // i18n.changeLanguage('en') // to change language
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{t('Welcome to React')}</h1>
     </div>
   );
 }
